651f560e517ccde362b10d591713cb348d64776a
Eduardo Przysiezny <eduardo_thp@yahoo.com.br>
2018-07-06 17:46:42 -0500

    04-Exercise completion

--- app/src/main/java/android/example/com/squawker/MainActivity.java
+++ app/src/main/java/android/example/com/squawker/MainActivity.java
@@ -88,39 +88,22 @@ public class MainActivity extends AppCompatActivity implements
         // Start the loader
         getSupportLoaderManager().initLoader(LOADER_ID_MESSAGES, null, this);
 
-        // TODO (1) Make a new Service in the fcm package that extends from FirebaseMessagingService.
-        // TODO (2) As part of the new Service - Override onMessageReceived. This method will
-        // be triggered whenever a squawk is received. You can get the data from the squawk
-        // message using getData(). When you send a test message, this data will include the
-        // following key/value pairs:
-        // test: true
-        // author: Ex. "TestAccount"
-        // authorKey: Ex. "key_test"
-        // message: Ex. "Hello world"
-        // date: Ex. 1484358455343
-        // TODO (3) As part of the new Service - If there is message data, get the data using
-        // the keys and do two things with it :
-        // 1. Display a notification with the first 30 character of the message
-        // 2. Use the content provider to insert a new message into the local database
-        // Hint: You shouldn't be doing content provider operations on the main thread.
-        // If you don't know how to make notifications or interact with a content provider
-        // look at the notes in the classroom for help.
-
-        // TODO (5) You can delete the code below for getting the extras from a notification message,
+        // DONE (5) You can delete the code below for getting the extras from a notification message,
         // since this was for testing purposes and not part of Squawker.
-
-        // DONE (1) Get the test data here from the extras bundle that came with this intent.
-        // To confirm that the data was passed in, make sure to show the data in a log statement.
-        Bundle bundle = getIntent().getExtras();
-
-        // Gets the extra data from the intent that started the activity. For *notification*
-        // messages, this will contain key value pairs stored in the *data* section of the message.
-
-        // Checks if the extras exist and if the key "test" from our FCM message is in the intent
-        if (bundle != null && bundle.containsKey("test")) {
-            String testValue = bundle.getString("test");
-            Log.d(LOG_TAG, "Received value: " + testValue);
-        }
+        // EDDIE: I didn't really want to delete, so I just commented out
+//
+//        // DONE (1) Get the test data here from the extras bundle that came with this intent.
+//        // To confirm that the data was passed in, make sure to show the data in a log statement.
+//        Bundle bundle = getIntent().getExtras();
+//
+//        // Gets the extra data from the intent that started the activity. For *notification*
+//        // messages, this will contain key value pairs stored in the *data* section of the message.
+//
+//        // Checks if the extras exist and if the key "test" from our FCM message is in the intent
+//        if (bundle != null && bundle.containsKey("test")) {
+//            String testValue = bundle.getString("test");
+//            Log.d(LOG_TAG, "Received value: " + testValue);
+//        }
 
         // DONE (3) Here, in MainActivity, get a token using FirebaseInstanceId.getInstance().getToken()
         // DONE (4) Get the message from that token and print it in a log statement
